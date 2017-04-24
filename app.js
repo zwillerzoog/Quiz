be82cb15fe9485abb972e728c0e4ef84812fc3c0
Zwillerzoog <marsh.heidi.c@gmail.com>
2017-04-23 22:22:11 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log("hello");
+
+function showAnswer() {
+	if $(".wrong").on(click) {
+		$(".answer, .wrong").toggle();
+		$(".question").hide();
+	}
+
+	else $(".right").on(click) {
+		$(".answer, .right").toggle();
+		$(".question").hide();
+	}
+}

f416a135300418d87026be77e7e0698b0ddb8d2e
Zwillerzoog <marsh.heidi.c@gmail.com>
2017-04-25 16:36:25 -0400

    second push

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 console.log("hello");
 
-function showAnswer() {
-	if $(".wrong").on(click) {
-		$(".answer, .wrong").toggle();
-		$(".question").hide();
+$(document).ready(function(){
+	function show Question {
+		if $(".start").click(function(){
+			$(".question 1").show();
+		});
 	}
+	function showAnswer() {
+		if $(".wrong").click(function(){
+			$(".question").hide();
+			$(".incorrect").show();
+		});
+	};
 
-	else $(".right").on(click) {
-		$(".answer, .right").toggle();
-		$(".question").hide();
-	}
-}
+});

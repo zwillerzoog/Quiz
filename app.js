89dadf46c46556c06d9d0a0fa94e49a3604e88f1
Zwillerzoog <marsh.heidi.c@gmail.com>
2017-04-28 14:41:04 -0400

    fifth commit

--- app.js
+++ app.js
@@ -2,29 +2,38 @@ console.log("hello");
 
 
 
+function rightClickHandler(){
+	console.log("Button Clicked");
+	$(".correct").removeClass("hidden");
+	$(".incorrect").addClass("hidden");
+	
+}
+
+function wrongClickHandler(){
+	console.log("Oops!")
+	$(".incorrect").removeClass("hidden");
+	$(".correct").addClass("hidden");
+}
+
+function addClickHandler() {
+	
+
+	$(".right").click(rightClickHandler);
+
+	$(".wrong").click(wrongClickHandler);
+
+	$(".next").click(function() {
+		var currentQuestion = $(".question").not(".hidden");
+		currentQuestion.toggleClass("hidden");
+		currentQuestion.next().toggleClass("hidden");
+		$(".incorrect").addClass("hidden");
+		$(".correct").addClass("hidden");
+	});
+};
+
 $(document).ready(function(){
 	
 	console.log("In Document Ready");
 	addClickHandler();
 
 });
-
-function rightClickHandler(){
-	console.log("Button Clicked");
-	$(".correct").toggleClass("hidden");
-}
-
-function addClickHandler() {
-	
-	$(".right").click(rightClickHandler);
-
-	$(".wrong").click(function() {
-		$(".incorrect").toggleClass("hidden");
-	});
-
-	$(".next").click(function() {
-		var currentQuestion = $(".question").not(".hidden");
-		currentQuestion.toggleClass("hidden");
-		currentQuestion.next().toggleClass("hidden");
-	});
-};
